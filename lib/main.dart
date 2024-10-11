796af9dc78cb97a085449507b3e67bb6f04b4c0b
Yosralk <mmhuda46@gmail.com>
2024-10-11 21:43:11 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,69 @@
 import 'package:flutter/material.dart';
-main(){
-  runApp(TextWidget());
+
+void main(){
+  runApp(Yoser());
 }
-class TextWidget extends StatelessWidget{
+class Yoser extends StatefulWidget {
+  const Yoser({super.key});
+
+  @override
+  State<Yoser> createState() => _YoserState();
+}
+
+class _YoserState extends State<Yoser> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.green.shade600,
-          title: Text("TextWidget",
-              style: TextStyle(
-              fontSize: 50,
-                color: Colors.white,
-          ),
-            
-          ),
-      ),
-        body: Center(
-          child: RichText(text: TextSpan(
-            text: "hello" ,
-            style: TextStyle(
-              color: Colors.pink.shade300,
-            )
-          )
-          ),
+          title: Text("view1"),
+          backgroundColor: Colors.purpleAccent,
         ),
-    ),
+      body:ListView(
+        children: [
+          ListTile(
+            title: Text("book"),
+            leading: Icon(Icons.directions_walk),
+          ),
+          ListTile(
+            title: Text("book2"),
+            leading: Icon(Icons.directions_walk),
+          ),
+          ListTile(
+            title: Text("book3"),
+            leading: Icon(Icons.directions_walk),
+          ),
+          ListTile(
+            title: Text("book4"),
+            leading: Icon(Icons.directions_walk),
+          ),
+          ListTile(
+            title: Text("book5"),
+            leading: Icon(Icons.directions_walk),
+          ),
+          ListTile(
+            title: Text("book6"),
+            leading: Icon(Icons.directions_walk),
+          ),
+          ListTile(
+            title: Text("book7"),
+            leading: Icon(Icons.directions_walk),
+          ),
+          ListTile(
+            title: Text("book8"),
+            leading: Icon(Icons.directions_walk),
+          ),
+          ListTile(
+            title: Text("book9"),
+            leading: Icon(Icons.directions_walk),
+          ),
+          ListTile(
+            title: Text("book10"),
+            leading: Icon(Icons.directions_walk),
+          ),
+        ],
+      )
+      ),
     );
   }
 }
